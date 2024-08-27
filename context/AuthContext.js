6190063d6a814f736a0fc463024a7796f0103883
Aldair Ramos <raldair7@gmail.com>
2024-08-27 16:06:31 -0600

    fix error context 2

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -6,7 +6,7 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  const [loading, serLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
