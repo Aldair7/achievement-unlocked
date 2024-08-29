19de256b2be64efb0d97a6ab768413c8a800ffdf
Aldair Ramos <raldair7@gmail.com>
2024-08-28 22:53:56 -0600

    login.js  update

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -2,30 +2,45 @@ import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../lib/firebase";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState();
   const { user } = useAuth();
+  const router = useRouter();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
+      router.push("/dashboard");
     } catch (error) {
-      console.error("Error Logging in:", error);
+      setError("Invalid email or password");
     }
   };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Login</h1>
       {user ? (
         <p>welcome back, {user.email}!</p>
       ) : (
         <form onSubmit={handleLogin}>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-          <button type="submit">Login</button>
+          <input type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+            className="p-2 border border-gray-300 rounded" />
+          <input type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+            className="p-2 border border-gray-300 rounded"
+          />
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
         </form>
       )
       }
